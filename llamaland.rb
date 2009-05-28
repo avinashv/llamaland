0d05052a23d967ae60d7a539ac6b1bcf27ee5bcc
Avinash Vora <mail@avinashv.net>
2009-05-28 19:06:14 +0400

    Added README for Github.

--- llamaland.rb
+++ llamaland.rb
@@ -3,8 +3,6 @@ require 'sinatra'
 require 'sequel'
 require 'haml'
 
-encoding_base = 36
-
 configure do
     DB = Sequel.sqlite
     DB.create_table :urls do
@@ -23,10 +21,10 @@ get '/' do
 end
 
 get '/:shorturl' do
-    redirect DB[:urls].first(:id => params[:shorturl].to_i(encoding_base))[:url]
+    redirect DB[:urls].first(:id => params[:shorturl].to_i(36))[:url]
 end
 
 post '/' do
     id = DB[:urls].insert(:url => params[:url])
-    "#{request.url + id.to_s(encoding_base)}"
+    "#{request.url + id.to_s(36)}"
 end
